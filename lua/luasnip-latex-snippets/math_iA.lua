43d0a57bd5c33e28011402beafa4bc0069b303ac
Nijigokoro <92746684+Nijigokoro@users.noreply.github.com>
2024-09-26 22:24:26 +0200

    Update math_iA.lua

--- lua/luasnip-latex-snippets/math_iA.lua
+++ lua/luasnip-latex-snippets/math_iA.lua
@@ -92,8 +92,10 @@ function M.retrieve(is_math)
 
     parse_snippet({ trig = "td", name = "to the ... power ^{}" }, "^{$1}$0 "),
     parse_snippet({ trig = "rd", name = "to the ... power ^{()}" }, "^{($1)}$0 "),
-    parse_snippet({ trig = "vec", name = "Vector" }, "\\overrightarrow{($1)}$0 "),
-    parse_snippet({ trig = "bin", name = "Binomial" }, "\\binom{($2)}{($1)}$0 "),
+    parse_snippet({ trig = "vec", name = "Vector" }, "\\overrightarrow{$1}$0 "),
+    parse_snippet({ trig = "bin", name = "Binomial" }, "\\binom{$1}{$2}$0 "),
+    parse_snippet({ trig = "ubr", name = "Underbar" }, "\\underbar{$1}_{$2}$0 "),
+    parse_snippet({ trig = "mscr", name = "Script" }, "\\mathscr{$1}$0 "),
     parse_snippet({ trig = "cb", name = "Cube ^3" }, "^3 "),
     parse_snippet({ trig = "sr", name = "Square ^2" }, "^2"),
 
